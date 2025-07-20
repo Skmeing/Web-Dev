fabfa20ced46944721c68871e8c150bf577c686a
Skmeing <jbramley135@gmail.com>
2025-07-19 22:20:16 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<div id="header">
+  <img src="img/banner.JPG" alt="CAC banner" id="banner" />
+  <h1>Computing &amp; Analytics Center (CAC)</h1>
+
+  <nav>
+    <ul>
+      <li><a href="index.php"       class="menu">Home</a></li>
+      <li><a href="Faculty.php"     class="menu">Faculty</a></li>
+      <li><a href="projects.php"    class="menu">Projects</a></li>
+      <li><a href="eligibility.php" class="menu">Eligibility</a></li>
+    </ul>
+  </nav>
+</div>
