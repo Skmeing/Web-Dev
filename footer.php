51b04532ec0124d169dd768cd8d56bdd3953bc0c
Skmeing <jbramley135@gmail.com>
2025-07-19 22:38:35 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <div id="footer">
-  <p>
-    This site is a class project for
-    <a href="http://it5443.azurewebsites.net">IT&nbsp;5443</a>.
+  <p>This site is a class project for
+     <a href="http://it5443.azurewebsites.net">IT&nbsp;5443</a>.
   </p>
 </div>
